4acf5eb39d98d37485306ca197f7860e2da4ba11
RobertoCastillo <rcastillo1031@gmail.com>
2015-03-31 22:55:38 -0400

    Revert

--- views/js/main.js
+++ views/js/main.js
@@ -505,7 +505,6 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-	console.log(phase, body.scrollTop / 1250)
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
