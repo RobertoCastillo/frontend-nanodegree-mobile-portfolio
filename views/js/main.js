2ba73137c3da1691403d745b23ca413a42ae10db
RobertoCastillo <rcastillo1031@gmail.com>
2015-04-01 21:20:25 -0400

    Reduced pizza to 30

--- views/js/main.js
+++ views/js/main.js
@@ -529,7 +529,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 40; i++) {			//reduced to 40
+  for (var i = 0; i < 30; i++) {			//reduced to 40
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
